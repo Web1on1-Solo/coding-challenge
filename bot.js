69a22d4d86e7d3fa4782c48f41d029c3cadf47ca
Alejandro Morales <alex@moralestapia.com>
2022-07-13 17:01:49 +0000

    I set up my account at web1on1, created a new bot and got the neccessary API and webhook keys to be able to work.
    I also set up a crude express server to verify the webhook and to hook a chipchat instance to it.
    Comment: I wasn't able to suer the chipchat as middleware, but the "raw ingest" approach seems to be working for now.
    
    I set up a very basic bot structure just to see if I am getting messages correctly.

--- bot.js
+++ bot.js
@@ -1,15 +1,58 @@
-const ChipChat = require('chipchat');
-const bot = new ChipChat({ token: process.env.TOKEN });
+// const ChipChat = require('chipchat');
+// const bot = new ChipChat({ token: process.env.TOKEN });
 
-// conversation assigned to the bot
-bot.on('assign', async (m, c) => {
-    console.log('', m.text);
-    c.say('Hello agent');
-});
+// bot.on('assign', async (m, c) => {
+// 	console.log('', m.text);
+// 	c.say('Hello agent');
+// });
 
-// contact chat message (in a contact conversation)
-bot.on('message.create.contact.chat.contact', (message, conversation) => {
-    conversation.say('Hey, consumer', { role: 'agent' });
-});
+// bot.on('message.create.contact.chat.contact', (message, conversation) => {
+// 	conversation.say('Hey, consumer', { role: 'agent' });
+// });
 
-bot.start();
+// bot.start();
+
+const CONFIG = require('./config.js');
+
+const MODULE = {
+	CHIPCHAT:require('chipchat'),
+	EXPRESS:require('express'),
+	FS:require('fs'),
+	URL:require('url')
+};
+
+MODULE.FS.watchFile(__filename, { internval:500 }, () => process.exit(0));
+
+console.log(Date.now());
+
+const BOT = new MODULE.CHIPCHAT({
+		email:CONFIG.WEB1ON1.BOT.clientId,
+		refreshToken:CONFIG.WEB1ON1.BOT['Refresh Token'],
+		secret:CONFIG.WEB1ON1.BOT.WEBHOOK.SECRET,
+		token:CONFIG.WEB1ON1.BOT['API Token']
+	});
+
+MODULE.EXPRESS()
+	// .use(BOT.router()) // <-- I WAS NOT ABLE TO SET IT UP LIKE THIS, PERHAPS THE DOCS NEED A REVISION
+	.use(MODULE.EXPRESS.json())
+
+	.get('/web1on1/webhook/', (req, res) => {
+		req.url = MODULE.URL.parse(req.url, true);
+
+		if(req.url.query.type == 'subscribe'&&req.url.query.challenge != null) res.status(200).append('Content-Type', 'text/plain').send(req.url.query.challenge);
+		else res.status(400).end();
+	})
+
+	.post('/web1on1/webhook/', (req, res) => {
+			BOT.ingest(req.body); // ? - DOES THE NODE SDK PERFORM VERIFICATION AUTOMATICALLY?
+
+			res.status(200).send();
+		})
+
+
+	.listen(CONFIG.EXPRESS.PORT);
+
+BOT
+	.on('message', (message, conversation) => {
+			console.log('message', message);
+		});
