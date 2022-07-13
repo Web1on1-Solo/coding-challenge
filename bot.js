40f88b309217c46e40064028aad6e06d300a4759
Alejandro Morales <alex@moralestapia.com>
2022-07-13 20:37:23 +0000

    Added two functions AES256_CIPHER and AES256_DECIPHER, which are very crude implementations of aes256 (i.e. no secuiry guarantees) but they are needed to build a proof-of-concept about how to exchange messages between a webview, a bot's backend and a web1on1 conversation.
    I will keep working on the complementary webview on the backend, that makes all of this work. This webview is going to be used as the date/time picker for the appointment.

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const CONFIG = require('./config.js');
 
 const MODULE = {
 	CHIPCHAT:require('chipchat'),
+	CRYPTO:require('crypto'),
 	EXPRESS:require('express'),
 	FS:require('fs'),
 	HTTPS:require('https'),
@@ -43,9 +44,28 @@ const HTTPS_POST_PATCH_MESSAGE = async args => new Promise(done => {
 		r.end(JSON.stringify({ text:args.text }));
 	});
 
+// THESE TWO FUNCTIONS, AES256_CIPHER (TO SERIALIZE) AND AES256_DECIPHER (TO DESERIALIZE=, ARE MEANT TO PACK DATA IN A 'SECURE' MANNER SO THAT WE CAN EXCHANGE MESSAGES BETWEEN THE WEBVIEW AND THE BACKEND AND EXTEND THE CONVERSATION THERE
+const AES256_CIPHER = data => {
+	var iv = MODULE.CRYPTO.randomBytes(16);
+	var e = MODULE.CRYPTO.createCipheriv('aes256', '01234567890123456789012345678901', iv);
+
+	return Buffer.from(JSON.stringify({ data:e.update(JSON.stringify(data), 'utf8', 'base64') + e.final('base64'), iv:iv.toString('base64') })).toString('base64url');
+};
+
+const AES256_DECIPHER = data => {
+	try {
+		data = JSON.parse(Buffer.from(data, 'base64url'));
+
+		var d = MODULE.CRYPTO.createDecipheriv('aes256', '01234567890123456789012345678901', Buffer.from(data.iv, 'base64'));
+		return JSON.parse(d.update(data.data, 'base64', 'utf8') + d.final('utf8'));
+	} catch(e) {}
+};
+
 MODULE.EXPRESS()
 	// .use(BOT.router()) // <-- I WAS NOT ABLE TO SET IT UP LIKE THIS, PERHAPS THE DOCS NEED A REVISION
 	.use(MODULE.EXPRESS.json())
+
+	.get('/misc/datepicker', (req, res) => MODULE.FS.readFile(__dirname + '/misc/datepicker/index.html', (e, d) => res.status(200).append('Content-Type', 'text/html;charset=utf-8').send(d)))
 
 	.get('/web1on1/webhook/', (req, res) => {
 		req.url = MODULE.URL.parse(req.url, true);
@@ -56,6 +76,18 @@ MODULE.EXPRESS()
 
 	.post('/web1on1/webhook/', (req, res) => {
 		BOT.ingest(req.body); // ? - DOES THE NODE SDK PERFORM PAYLOAD VERIFICATION AUTOMATICALLY? I ASSUME YES, BUT HAVE TO CHECK IT OUT
+
+		res.status(200).send();
+	})
+
+	.post('/web1on1/webhook/appointment/setup', (req, res) => {
+		console.log('data', req.body);
+
+		if(req.body.data != null) {
+			req.body.data.data = AES256_DECIPHER(req.body.data.data);
+
+			if(req.body.data.data != null&&req.body.data.data.conversation_id != null) BOT.send(req.body.data.data.conversation_id, { text:Date.now() + ':TIME AND DATE SET ...' }).then(d => console.log('DONE ...'));
+		}
 
 		res.status(200).send();
 	})
